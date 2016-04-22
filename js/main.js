54477246b25f8015e1b3f84ed58d25e7d29e6f2e
arial-alisha <aria0023@algonquinlive.com>
2016-04-22 10:51:46 -0400

    trying to set up repository

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var $body = $('body');
+var $rightside = $('#right-hem');
+var $leftmag = $('#left-mag');
+
+$right-hem.on('click', '#right-hem', function () {
+  $leftmag.toggleClass ('left-mag-move');
+});
