e53032ddad9a3ab90c56a3592a7819219f95aee7
arial-alisha <aria0023@algonquinlive.com>
2016-04-27 18:01:00 -0400

    waypoints, brain disappeared

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
 var $body = $('body');
 var $rightside = $('#right-hem');
 var $leftmag = $('#left-mag');
+var $rightmag = $('#right-mag');
+var $leftside = $('#left-hem');
+var $brain = $('.brain-box');
 
 $body.on('click', '#right-hem', function () {
-  $l('left-mag').toggleClass ('left-mag-move');
+  $leftmag.toggleClass ('left-mag-move');
 });
+
+$body.on('click'), '#left-hem', function () {
+  $rightmag.toggleClass ('right-mag-move');
+}
+
+$brain.waypoint (function (direction) {
+  if (direction == 'down') {
+    $brain.addClass('js-brain-animate')
+  } else {
+    $brain.addClass('js-brain-animate')
+  }
+}, {offset: '50%'});
