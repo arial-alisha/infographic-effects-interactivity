50939ebeeda7d67232230a690f4e161c8b82c0ca
arial-alisha <aria0023@algonquinlive.com>
2016-04-28 21:54:27 -0400

    Markbot fixes

--- js/main.js
+++ js/main.js
@@ -20,113 +20,113 @@ var $image12 = $('.image12');
 $('nav').localScroll();
 
 $body.on('click', '#right-hem', function () {
-  $leftmag.toggleClass ('left-mag-move');
+  $leftmag.toggleClass('left-mag-move');
 });
 
 $body.on('click', '#left-hem', function () {
-  $rightmag.toggleClass ('right-mag-move');
+  $rightmag.toggleClass('right-mag-move');
 });
 
-$brain.waypoint (function (direction) {
+$brain.waypoint(function (direction) {
   if (direction == 'down') {
-    $brain.addClass('js-brain-animate')
+    $brain.addClass('js-brain-animate');
   } else {
-    $brain.addClass('js-brain-animate')
+    $brain.addClass('js-brain-animate');
   }
 }, {offset: '50%'});
 
-$image1.waypoint (function (direction) {
+$image1.waypoint(function (direction) {
   if (direction == 'down') {
-    $image1.addClass('js-brain-animate')
+    $image1.addClass('js-brain-animate');
   } else {
-    $image1.addClass('js-brain-animate')
+    $image1.addClass('js-brain-animate');
   }
 }, {offset: '70%'});
 
-$image2.waypoint (function (direction) {
+$image2.waypoint(function (direction) {
   if (direction == 'down') {
-    $image2.addClass('js-brain-animate')
+    $image2.addClass('js-brain-animate');
   } else {
-    $image2.addClass('js-brain-animate')
+    $image2.addClass('js-brain-animate');
   }
 }, {offset: '70%'});
 
-$image3.waypoint (function (direction) {
+$image3.waypoint(function (direction) {
   if (direction == 'down') {
-    $image3.addClass('js-brain-animate')
+    $image3.addClass('js-brain-animate');
   } else {
-    $image3.addClass('js-brain-animate')
+    $image3.addClass('js-brain-animate');
   }
 }, {offset: '70%'});
 
-$image4.waypoint (function (direction) {
+$image4.waypoint(function (direction) {
   if (direction == 'down') {
-    $image4.addClass('js-brain-animate')
+    $image4.addClass('js-brain-animate');
   } else {
-    $image4.addClass('js-brain-animate')
+    $image4.addClass('js-brain-animate');
   }
 }, {offset: '70%'});
 
-$image5.waypoint (function (direction) {
+$image5.waypoint(function (direction) {
   if (direction == 'down') {
-    $image5.addClass('js-brain-animate')
+    $image5.addClass('js-brain-animate');
   } else {
-    $image5.addClass('js-brain-animate')
+    $image5.addClass('js-brain-animate');
   }
 }, {offset: '70%'});
 
-$image6.waypoint (function (direction) {
+$image6.waypoint(function (direction) {
   if (direction == 'down') {
-    $image6.addClass('js-brain-animate')
+    $image6.addClass('js-brain-animate');
   } else {
-    $image6.addClass('js-brain-animate')
+    $image6.addClass('js-brain-animate');
   }
 }, {offset: '70%'});
 
-$image7.waypoint (function (direction) {
+$image7.waypoint(function (direction) {
   if (direction == 'down') {
-    $image7.addClass('js-brain-animate')
+    $image7.addClass('js-brain-animate');
   } else {
-    $image7.addClass('js-brain-animate')
+    $image7.addClass('js-brain-animate');
   }
 }, {offset: '70%'});
 
-$image8.waypoint (function (direction) {
+$image8.waypoint(function (direction) {
   if (direction == 'down') {
-    $image8.addClass('js-brain-animate')
+    $image8.addClass('js-brain-animate');
   } else {
-    $image8.addClass('js-brain-animate')
+    $image8.addClass('js-brain-animate');
   }
 }, {offset: '70%'});
 
-$image9.waypoint (function (direction) {
+$image9.waypoint(function (direction) {
   if (direction == 'down') {
-    $image9.addClass('js-brain-animate')
+    $image9.addClass('js-brain-animate');
   } else {
-    $image9.addClass('js-brain-animate')
+    $image9.addClass('js-brain-animate');
   }
 }, {offset: '70%'});
 
-$image10.waypoint (function (direction) {
+$image10.waypoint(function (direction) {
   if (direction == 'down') {
-    $image10.addClass('js-brain-animate')
+    $image10.addClass('js-brain-animate');
   } else {
-    $image10.addClass('js-brain-animate')
+    $image10.addClass('js-brain-animate');
   }
 }, {offset: '70%'});
 
-$image11.waypoint (function (direction) {
+$image11.waypoint(function (direction) {
   if (direction == 'down') {
-    $image11.addClass('js-brain-animate')
+    $image11.addClass('js-brain-animate');
   } else {
-    $image11.addClass('js-brain-animate')
+    $image11.addClass('js-brain-animate');
   }
 }, {offset: '70%'});
 
-$image12.waypoint (function (direction) {
+$image12.waypoint(function (direction) {
   if (direction == 'down') {
-    $image12.addClass('js-brain-animate')
+    $image12.addClass('js-brain-animate');
   } else {
-    $image12.addClass('js-brain-animate')
+    $image12.addClass('js-brain-animate');
   }
 }, {offset: '80%'});
